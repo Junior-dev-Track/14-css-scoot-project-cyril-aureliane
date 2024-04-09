bfe14371c7c2c5a2a60ed72c2912f753c8024a0c
cyril-fl <cyril-f@hotmail.com>
2024-04-09 18:15:46 +0200

    * includes upgrade ** inner links nav & footer fonctional

--- ScootProject/scripts/include.js
+++ ScootProject/scripts/include.js
@@ -1,9 +1,23 @@
 // Fonction asynchrone permettant d'inclure un fichier HTML dans un autre à l'aide de fetch et innerHTML
-async function include(ref) {
+async function includeFonts(ref) {
     try {
         const response = await fetch(`./component/${ref}.html`);
         const content = await response.text();
         document.body.innerHTML += content;
+    } catch (error) {
+        console.error("Une erreur s'est produite lors du chargement du fichier :", error);
+    }
+}
+
+
+// Fonction asynchrone permettant d'inclure un fichier HTML dans un autre à l'aide de fetch et innerHTML
+async function include(ref, parentSelector) {
+    try {
+        const response = await fetch(`./component/${ref}`);
+        const content = await response.text();
+
+        document.querySelector(parentSelector).innerHTML += content;
+
     } catch (error) {
         console.error("Une erreur s'est produite lors du chargement du fichier :", error);
     }
