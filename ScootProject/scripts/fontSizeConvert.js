7d40056599b07e559d68b5fb1cde5874b55310a0
cyril-fl <cyril-f@hotmail.com>
2024-04-10 09:32:41 +0200

    * -

--- ScootProject/scripts/fontSizeConvert.js
+++ ScootProject/scripts/fontSizeConvert.js
@@ -1,8 +1,15 @@
 // Unit
-let fontSize=15;
-let lineHeight=25;
-let letterSpacing=-2.14;
+let fontSize=56;
+let lineHeight=56;
+let letterSpacing=-2.5;
 
+let screenSize = [576, 768, 1024, 1226, 1440 ]
+let [phone, ipadMini, tablet, laptop, desktop ] = screenSize
+
+
+function responsiveSize(size, ) {
+
+}
 
 
 // Function
@@ -21,3 +28,4 @@ function converter(fontSize,lineHeight,letterSpacing){
 }
 converter(fontSize,lineHeight,letterSpacing)
 
+// faire une convertion px -> vw
