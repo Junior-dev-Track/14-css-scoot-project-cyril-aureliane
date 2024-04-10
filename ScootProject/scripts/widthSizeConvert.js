102a9c9b82a8a26cba37d71caca9f31004b2f66e
cyril-fl <cyril-f@hotmail.com>
2024-04-10 09:43:04 +0200

    * widthSizeConvert.js Init

---
+++ ScootProject/scripts/widthSizeConvert.js
@@ -0,0 +1,19 @@
+// Convertion px -> vw
+// * Function
+function responsiveSize(size,screenSize) {
+
+    let responsive = (size/screenSize)*100
+    let result = `width: ${responsive.toFixed(3)}vw;`
+    console.log(result)
+}
+// ** Unit
+let screenSize = [576, 768, 1024, 1226, 1440 ]
+let [   phone,
+        ipadMini,
+        tablet,
+        laptop,
+        desktop ] = screenSize
+
+// ** Call to fonction
+
+responsiveSize(100, phone)
