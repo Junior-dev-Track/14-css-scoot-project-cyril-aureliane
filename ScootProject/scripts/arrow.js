c50c25774bd320f25fd12124a9a03bc51e5ba216
cyril-fl <cyril-f@hotmail.com>
2024-04-12 12:23:56 +0200

    * arrow on click

--- ScootProject/scripts/arrow.js
+++ ScootProject/scripts/arrow.js
@@ -1,8 +1,4 @@
 function arrowClick(element) {
-
-    alert('hello ' + id + ' from arrow.js');
-
-
     let thisArrow = element;
     let thisParent = element.parentNode;
     let thisGrandParentElementId = thisParent.parentNode.id;
@@ -14,22 +10,6 @@ function arrowClick(element) {
     console.log(paragraph);
     console.log(paragraph.classList)
 
-
-
-
-
-
-    // let thisParent = document.getElementById(thisParentId);
-
-    // console.log(paragraph.textContent); // Affiche le contenu texte du paragraphe
-
-
-
-
-    // console.log(thisParent);
-
-  // // let thisArrow = document.getElementById(id);
-  //   paragraph.classList.toggle('none')
     setTimeout(() => {
         paragraph.classList.toggle('none')
     }, 350)
@@ -37,27 +17,4 @@ function arrowClick(element) {
     thisArrow.classList.toggle('down')
     paragraph.classList.toggle('hide');
     paragraph.classList.toggle('show');
-
-
-
-
-
-
 }
-
-
-// $(document).on('click', '.burger', function() {
-//     $(this).toggleClass('active');
-//     $('nav').toggleClass('switch');
-//     $('body').toggleClass('overflowNone');
-//
-// });
-
-// <script>
-//     const menuHamburger = document.querySelector(".menu-hamburger")
-//     const navLinks = document.querySelector(".nav-links")
-//
-//     menuHamburger.addEventListener('click',()=>{
-//     navLinks.classList.toggle('mobile-menu')
-// })
-// </script>
